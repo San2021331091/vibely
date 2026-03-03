76c07354fbfe47e5f894464affe853ad92dd25df
San2021331091 <2021331091@student.sust.edu>
2026-03-03 15:30:00 +0600

    new commit

---
+++ lib/widgets/input_text_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InputTextWidget extends StatefulWidget {
+  const InputTextWidget({super.key});
+
+  @override
+  State<InputTextWidget> createState() => _InputTextWidgetState();
+}
+
+class _InputTextWidgetState extends State<InputTextWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
