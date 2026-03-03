e3e5ae1f1f7ca0678db6ff58c97c794afa5e0ee9
San2021331091 <2021331091@student.sust.edu>
2026-03-03 15:42:23 +0600

    new commit

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:vibely/widgets/input_text_widget.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -9,6 +10,9 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passWordTextEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,30 +20,45 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Center(
           child: Column(
             children: [
-              SizedBox(height: 100,),
-            
+              SizedBox(height: 100),
+
               Image.network(
                 "https://i.postimg.cc/XYwxWd41/tune.png",
                 width: 200,
               ),
-              Text("Welcome",
-               style: GoogleFonts.acme(
-                fontSize: 34,
-                color: Colors.grey,
-                fontWeight: FontWeight.bold 
-               ),
-            ),
-            Text("Glad to see you",
-               style: GoogleFonts.abhayaLibre(
-                fontSize: 34,
-                color: Colors.grey,
-               ),
-               
-            ),
-            SizedBox(height: 30,),
-            ],
+              Text(
+                "Welcome",
+                style: GoogleFonts.acme(
+                  fontSize: 34,
+                  color: Colors.grey,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "Glad to see you",
+                style: GoogleFonts.abhayaLibre(
+                  fontSize: 34,
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(height: 30),
 
-            
+              //email input
+              InputTextWidget(
+                textEditingController: emailTextEditingController,
+                labelString: "Email",
+                icondata: Icons.email_outlined,
+                isObscure: false,
+              ),
+
+              //password input
+               InputTextWidget(
+                textEditingController: passWordTextEditingController,
+                labelString: "Password",
+                icondata: Icons.email_outlined,
+                isObscure: true,
+              ),
+            ],
           ),
         ),
       ),
--- lib/widgets/input_text_widget.dart
+++ lib/widgets/input_text_widget.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 
-class InputTextWidget extends StatefulWidget {
-  const InputTextWidget({super.key});
+class InputTextWidget extends StatelessWidget {
+  final TextEditingController textEditingController;
+  final IconData? icondata;
+  final String? assetReference;
+  final String labelString;
+  final bool isObscure;
 
-  @override
-  State<InputTextWidget> createState() => _InputTextWidgetState();
-}
+  const InputTextWidget({
+    super.key,
+    required this.textEditingController,
+    this.icondata,
+    this.assetReference,
+    required this.labelString,
+    required this.isObscure,
+  });
 
-class _InputTextWidgetState extends State<InputTextWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return TextField(
+      
+    );
   }
 }
